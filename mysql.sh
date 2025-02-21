4c8f157d9fa480a602d84d0b668259680707fc5b
Nanda4312 <nandag1516@gmail.com>
2025-02-21 15:02:39 +0530

    mysql script

--- mysql.sh
+++ mysql.sh
@@ -32,3 +32,24 @@ VALIDATE(){
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "installing MYSQL server"
+
+systemctl enable mysqld &>>$LOG_FILE
+Validate $? "enabled mysql server"
+
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Started MySQL server"
+
+mysql -h mysql.daws81s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting UP root password"
+else
+    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
+fi
+
